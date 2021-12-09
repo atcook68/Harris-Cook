790a0ff1f773d849cdf26816976a04162224869a
Andrew Cook <56707541+atcook68@users.noreply.github.com>
2021-12-08 22:57:02 -0800

    Add files via upload

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,89 +1,98 @@
+
+import types
 from players import AI, User
 import math
 import random
 
-class Game():
+# creating the Game class
+class Game():       
     def __init__(self):
         self.board = self.createBoard
-        self.victor = None
+        self.victor = None      # clearing the game victor
 
-# establishing the board and the victor.
-    def createBoard():     #   creating a board with 9 total places to fill.
-        return [' ',' ',' ',' ',' ',' ',' ',' ',' ',]   #for square in range(9)]
+    
+# establishing the board.
+    @staticmethod
+    def createBoard():
+        board = [[0, 0, 0,0,0],[0, 0, 0, 0, 0],[0, 0, 0, 0, 0],[0,0,0,0,0],[0,0,0,0,0]]
+             #   creating a board with 25 total places to fill.
+        return [' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ']   #for square in range(9)]
 
     def printBoard(self):                        # the function to print the board. 
-        for row in [self.theBoard[i * 3:
-            (i + 1) * 3]
+        for row in [self.board[i * 5:         # printing the rows of the spaces in the board. 
+            (i + 1) * 5]
             for i in range(3)]:
                 print(' '+' | '.join(row)+' ')        # concatenating the pipe strings to make the columns of the board.
 
-
-    def boardPlaces(self):
-        places = [[str(i) for i in range(a * 3, (a + 1)*3)]for a in range(3)]       # assigning numbers to the board places.
+    @staticmethod
+    def boardPlaces():
+        places = [[str(i) for i in range(a * 5, (a + 1)*5)]for a in range(5)]       # assigning numbers to the board places.
         for row in places:
             print('| '+' | '.join(row)+' |')                            # the program displays the 3x3 board in rows
 
-    def playerMoves(self,square,letter):        # making the player choose a space and letter 
-        if self.theBoard[square] == ' ':
-            self.theBoard[square] == letter
-            if self.won(square,letter):
-                self.victor = letter
+    def playerMoves(self,square,letter):        # making the player choose a space and letter corresponding to the board 
+        if self.board[square] == ' ':        # is the space empty?
+            self.board[square] == letter     # inputting the players character into the selected square
+            if self.won(square,letter):         # if the letters fit the win condition, 
+                self.victor = letter            # the letter player is declared the victor. 
             return True
         return False
 
-#   Are the moves ending the game? this function checks
+#   Are the moves ending the game? this method checks
     def victor(self,square,letter):
         def checkRow(self, square, letter):     # checking the the rows for same letter
             checkRow = math.floor(square/3)
-            row = self.theBoard[checkRow * 3: (checkRow + 1) * 3]
+            row = self.board[checkRow * 3: (checkRow + 1) * 5]
 
             if all([s == letter for s in row]):            # if player has occupied all of the places in the row, they are the victor.
                 return True                                 # true means victor
 
-            checkColumn = square % 3
-            column = [self.theBoard[checkColumn + i * 3] for i in range()]      # checking the column for the same letters
+            checkColumn = square % 5
+            column = [self.board[checkColumn + i * 5] for i in range()]      # checking the column for the same letters
             if all ([s== letter for s in column]):return True                   # if player has occupied all of the places in the column, they are the victor as well. 
 
             if square%2 == 0:       # if the squares are even,
-                winDiagonally = [self.board[i] for i in [0,4,8]]  # check for a win in from the bottom left to the top right. 
-                if all([s== letter for s in winDiagonally]):
+                winDiagonally = [self.board[i] for i in [0,6,12,18,24]]  # check for a win in from the bottom left to the top right. 
+                if all([s== letter for s in winDiagonally]):        # checks the boolean for all letters diagonally.
                     return True                                     # victory
 
-                winDiagonallyRight = [self.board[i] for i in [0,4,8]]  # check for a win diagonally right. 0,4,8 being from the top left to the bottom right
+                winDiagonallyRight = [self.board[i] for i in [0,6,12,18,24]]  # check for a win diagonally right. 0,4,8 being from the top left to the bottom right
                 if all ([s== letter for s in winDiagonallyRight]):
                     return True                                     # victory
-                winDiagonallyLeft = [self.board[i] for i in [2,4,6]]    # checks for a win diagonally left. 2,4,6 being from the top right to the bottom left.
+                winDiagonallyLeft = [self.board[i] for i in [4,8,12,16,20]]    # checks for a win diagonally left. 2,4,6 being from the top right to the bottom left.
                 if all ()([s== letter for s in winDiagonallyLeft]):
                     return True                                     # victory
-            return False
-                                                    # loss, if not
-    def emptySpace(self):
-        return ' ' in self.board
-    def spacesLeft(self):
-        return ' ' in self.board.count(' ')
-  #  def spacesLeft(self):
-  #      return [i for i, x in enumerate(self.board) if x == ' ']
-    def availableMoves(self):
+            return False                                            # loss, if not
+                        
+
+    def blank_spaces(self):
+        return ' '
+    def number_bs(self):                   # finding empty space on the board
+        return ' ' in self.board.count(' ') 
+    @staticmethod
+    def available_moves(self):
         return [i for i, x in enumerate(self.board) if x == " "]
 
 def current(game,x,o, printGame = True):     # establishing the game, and setting the players
         if printGame:
             game.boardPlaces()
         letter = 'x'
-        while game.emptySpace():
+        while game.blank_spaces():
             if letter == 'o':
                 square = o.get_move(Game)
             else:
                 square = x.get_move(Game)
+
+                # moving the player around the board and printing the board. 
             if game.make_move(square,letter):
                 if printGame:
-                    print(letter + ' makes a move to square {}'.format(square))
+                    print(letter + ' moves to square:  {}'.format(square))
                 game.print_board()
                 print('')
 
             if game.victor:
                 if printGame:
-                    print(letter + ' wins')
+                    print(letter + ' is the victor.')
                     tictactoe = 'tictactoe.txt'         
                     with open(tictactoe) as file_object:            # open the tictactoe text file.
                         file_object.write(letter + ' victory\n')    # record the win, whosever it is, to the file.
--- players.py
+++ players.py
@@ -1,5 +1,6 @@
 import random
 import math
+
 class Player:
         def __init__(self,letter):
             self.letter = letter
@@ -13,7 +14,7 @@ class User(Player):       # The player taking your input as the "human/user"
             valid = False
             val=None
             while val == False:
-                square = input(self.letter + '\' turn, where do you choose? (0 up to 0): ')
+                square = input(self.letter + '\' turn, where do you choose? (space 0 up to space 9): ')
                 try:
                     val = int(square)           # turns the value into an integer
                     if val not in game.spacesLeft():
@@ -23,29 +24,48 @@ class User(Player):       # The player taking your input as the "human/user"
                     print('Make another selection')
             return val
 
+class RandomComputerPlayer(Player):
+    def __init__(self, letter):
+        super().__init__(letter)
+
+    def get_move(self, game):
+        square = random.choice(game.available_moves())
+        return square
+
 class AI(Player):             # the AI making moves for the computer
         def __init__(self, letter):
             super().__init__(letter)
         def get_move(self, game):
-            if len.game.available_moves() == 9:        # if the board is empty, make a random choice
-                square = random.choice(game.available_moves())
+            if len(game.available_moves(self)) == 9:        # if the board is empty, make a random choice   ---------alterrrrr
+                square = random.choice(game.availableMoves())
             else:                                       # this is where the minimax algorith takes place
                 square = self.minimax(game, self.letter)['position']        # the minimax algorithm will choose the best choice given the empty spaces.
             return square
-        def minimax(self,state,player):                
-            max_player = self.letter
-            other_player = 'o' if player == 'x' else 'x'
-            if state.current_winner == other_player:            # if the last move won, return
+        def minimax(self,state,player):              
+            current_player = self.letter
+            opposition = 'o' if player == 'x' else 'x'
+            if state.current_winner == opposition:            # if the last move won, return
                 return {'position': None, 'score':1* (state.emptySpaces() + 1) 
-                if other_player == max_player else -1 * (state.num_empty_squares() + 1)}
+                if opposition == current_player else -1 * (state.num_empty_squares() + 1)}
             elif not state.empty_squares():            # if no empty squares are available
                 return {'position':None,'score':0}      # return a score of zero
 
-            if player == max_player:
-                optimal = {'position':None, 'score':-math.inf} 
+            if player == current_player:                                    #
+                optimal = {'position':None, 'score':-math.inf}              #
             else:
-                optimal = {'position': None,'score':math.inf}
-            for possible_move in state.available_moves():
-                state.make_move(possible_move,player)
-                score = self.minimax(state,other_player)
+                optimal = {'position': None,'score':math.inf}               #
+            for possible_move in state.availableMoves():                   #
+                state.make_move(possible_move,player)                       #
+                score = self.minimax(state,opposition)                      #
 
+                state.board[possible_move] = ' '
+                state.victor = None
+                score['position'] = possible_move
+
+                if player == AI:
+                    if score['score'] > best['score']:
+                        best = score
+                else:
+                    if score['score'] < best['score']:
+                        best = score
+            return best
